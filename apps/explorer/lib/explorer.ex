cb9466ee9b7a841dcc44545b782c2a43cd3b7fb2
MARTIN Pierre <HicksCorp@GMail.com>
2017-09-10 14:47:20 +0100

    Removes useless docs from Phoenix related files.

--- apps/explorer/lib/explorer.ex
+++ apps/explorer/lib/explorer.ex
@@ -1,21 +1,5 @@
 defmodule Explorer do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
-
-  This can be used in your application as:
-
-      use Explorer, :controller
-      use Explorer, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
+  @moduledoc false
 
   def controller do
     quote do
@@ -52,9 +36,6 @@ defmodule Explorer do
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/view/etc.
-  """
   defmacro __using__(which) when is_atom(which) do
     apply __MODULE__, which, []
   end
